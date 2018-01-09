0e2856d797cd098d12d361fdaf8b849bc203a319
Bearguy <stephen@peterkins.ca>
2018-01-09 17:06:05 -0500

    Order changes to events

--- routes/configRoute.js
+++ routes/configRoute.js
@@ -86,6 +86,7 @@ module.exports = (app) => {
                 if (payload === 'SOON') {
                     const events = await Event.findAll({
                         where: { startTime: { $gt: moment(timeOfMessage) }},
+                        order: [ ['startTime', 'DESC'] ],
                         limit: 5
                     });
 
